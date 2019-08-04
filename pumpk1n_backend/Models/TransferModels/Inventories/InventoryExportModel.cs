48747e27d45f88d90d699fc7c15bbe7f818e8e7f
Black Heat (undefined) <blackheatinc@gmail.com>
2019-08-05 00:34:48 +0700

    updated InventoryExportModel for null customer

--- pumpk1n_backend/Models/TransferModels/Inventories/InventoryExportModel.cs
+++ pumpk1n_backend/Models/TransferModels/Inventories/InventoryExportModel.cs
@@ -5,7 +5,7 @@ namespace pumpk1n_backend.Models.TransferModels.Inventories
 {
     public class InventoryExportModel
     {
-        public long CustomerId { get; set; }
+        public long? CustomerId { get; set; }
         public DateTime ExportedDate { get; set; } = DateTime.Now;
         public List<long> InventoryItems { get; set; }
     }
